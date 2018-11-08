fc414ec9d904cd09ad83693286b85c0c51831310
sarmacki <grzegorzeryks@gmail.com>
2018-11-09 00:48:06 +0100

    sound css and js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
 /*jshint esversion: 6 */
-let words = ['dog','cat','mouse','highway','fourty','seven'];
-let word = document.querySelector('.word');
-console.log(word);
+document.addEventListener("DOMContentLoaded", function(event) {
 
+let words = ['dog', 'cat', 'mouse', 'highway', 'fourty', 'seven'];
+let word = document.querySelector('.word');
+let letters = document.querySelectorAll('.letters div');
+let clickSound = new Audio('snd/click.mp3');
 
 //Function that randomly chooses an item from an array and splits it into new array
-function randomWord(){
+function randomWord() {
   let randomNr = Math.floor(Math.random() * words.length);
   let randomString = words[randomNr];
   let newArray = randomString.split('');
 
-for (var i = 0; i < newArray.length; i++) {
-  let newBox = document.createElement('div');
-  let newWord = document.createElement('span');
-  newWord.innerText = newArray[i].toUpperCase();
-
-  word.appendChild(newWord);
-}
+  for (var i = 0; i < newArray.length; i++) {
+    let newBox = document.createElement('div');
+    let newWord = document.createElement('span');
+    newWord.innerText = newArray[i].toUpperCase();
+    newBox.appendChild(newWord);
+    word.appendChild(newBox);
+  }
 
 }
 
 randomWord();
+
+
+//functions checking letters
+function checkLetter() {
+  console.log('works');
+  clickSound.play();
+  if (true) {
+
+  }
+  else {
+    console.log('wrong!');
+  }
+}
+
+for (var i = 0; i < letters.length; i++) {
+  letters[i].addEventListener('click', checkLetter);
+
+}
+
+
+});
