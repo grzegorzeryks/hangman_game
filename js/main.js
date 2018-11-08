a7d840c3796f92e6a724c2768584a1fea8276844
sarmacki <grzegorzeryks@gmail.com>
2018-11-09 00:26:38 +0100

    creating word function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 /*jshint esversion: 6 */
-console.log('elo');
 let words = ['dog','cat','mouse','highway','fourty','seven'];
+let word = document.querySelector('.word');
+console.log(word);
 
 
-
+//Function that randomly chooses an item from an array and splits it into new array
 function randomWord(){
   let randomNr = Math.floor(Math.random() * words.length);
   let randomString = words[randomNr];
- console.log(words[randomNr]);
   let newArray = randomString.split('');
-  console.log(newArray);
-};
+
+for (var i = 0; i < newArray.length; i++) {
+  let newBox = document.createElement('div');
+  let newWord = document.createElement('span');
+  newWord.innerText = newArray[i].toUpperCase();
+
+  word.appendChild(newWord);
+}
+
+}
 
 randomWord();
