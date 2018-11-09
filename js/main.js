ec659c96910cd27bd1c4b3f346cea569201a208d
sarmacki <grzegorzeryks@gmail.com>
2018-11-09 18:19:20 +0100

    getting the api

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 /*jshint esversion: 6 */
 document.addEventListener("DOMContentLoaded", function(event) {
 
+my = [];
+  fetch("https://my-json-server.typicode.com/grzegorzeryks/hangman_game/animals")
+    .then(res => res.json())
+    .then(res => {
+      for (var i = 0; i < res.length; i++) {
+        my.push(res[i]);
+      }
+      console.log(my);
+    });
+
+
+console.log(my);
+
   let words = ['dog', 'cat', 'mouse', 'highway', 'fourty', 'seven', 'bubble'];
   let word = document.querySelector('.word');
   let letters = document.querySelectorAll('.letters div');
@@ -48,7 +61,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
     }
   }
-// Adding checkLetter to all letter buttons/divs
+  // Adding checkLetter to all letter buttons/divs
   for (let i = 0; i < letters.length; i++) {
     letters[i].addEventListener('click', checkLetter);
   }
