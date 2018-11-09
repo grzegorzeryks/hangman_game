bf20b52fe60edf9eb0fb2063cef5558cac2c429e
sarmacki <grzegorzeryks@gmail.com>
2018-11-09 02:53:48 +0100

    letter checking function finished

--- js/main.js
+++ js/main.js
@@ -1,46 +1,53 @@
 /*jshint esversion: 6 */
 document.addEventListener("DOMContentLoaded", function(event) {
 
-let words = ['dog', 'cat', 'mouse', 'highway', 'fourty', 'seven'];
-let word = document.querySelector('.word');
-let letters = document.querySelectorAll('.letters div');
-let clickSound = new Audio('snd/click.mp3');
+  let words = ['dog', 'cat', 'mouse', 'highway', 'fourty', 'seven', 'bubble'];
+  let word = document.querySelector('.word');
+  let letters = document.querySelectorAll('.letters div');
+  let clickSound = new Audio('snd/click.mp3');
+  let newestArray;
 
-//Function that randomly chooses an item from an array and splits it into new array
-function randomWord() {
-  let randomNr = Math.floor(Math.random() * words.length);
-  let randomString = words[randomNr];
-  let newArray = randomString.split('');
+  //Function that randomly chooses an item from an array and splits it into new array and creates html elements
+  function randomWord() {
+    let randomNr = Math.floor(Math.random() * words.length);
+    let randomArray = words[randomNr].split('');
 
-  for (var i = 0; i < newArray.length; i++) {
-    let newBox = document.createElement('div');
-    let newWord = document.createElement('span');
-    newWord.innerText = newArray[i].toUpperCase();
-    newBox.appendChild(newWord);
-    word.appendChild(newBox);
+    for (var i = 0; i < randomArray.length; i++) {
+      let newBox = document.createElement('div');
+      let newWord = document.createElement('span');
+      newWord.innerText = randomArray[i].toUpperCase();
+      newBox.appendChild(newWord);
+      word.appendChild(newBox);
+    }
+    newestArray = randomArray;
   }
 
-}
+  randomWord();
+  console.log(newestArray);
+  let wordElements = document.querySelectorAll('.word div span');
 
-randomWord();
-
-
-//functions checking letters
-function checkLetter() {
-  console.log('works');
-  clickSound.play();
-  if (true) {
-
+  //function checking letters
+  function checkLetter() {
+    clickSound.play();
+    let isThere = false;
+    let letterNr = 0;
+    for (var i = 0; i < newestArray.length; i++) {
+      if (newestArray[i] === this.className) {
+        isThere = true;
+        letterNr = i;
+        wordElements[i].style.display = "block";
+      }
+    }
+    if (!isThere) {
+      console.log('nie ma');
+    } else {
+      console.log('jest');
+    }
   }
-  else {
-    console.log('wrong!');
+
+  for (var i = 0; i < letters.length; i++) {
+    letters[i].addEventListener('click', checkLetter);
   }
-}
-
-for (var i = 0; i < letters.length; i++) {
-  letters[i].addEventListener('click', checkLetter);
-
-}
 
 
 });
